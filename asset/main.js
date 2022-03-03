52032ad7f452cf16d8bd289dd952530d68371bd6
Valerio Corda <corda.valerio05@GMAIL.COM>
2022-03-03 21:35:33 +0100

    create a dropdown navbar menu

--- asset/main.js
+++ asset/main.js
@@ -1,11 +1,30 @@
-document.getElementById('dropdown-giochi').addEventListener('click', function name() {
+document.getElementById('dropdown-giochi').addEventListener('click', function openMenuGiochi() {
     document.getElementById('dropdown-giochi').classList.toggle('text-active-dropdown');
     document.getElementById('dropdown-submenu-giochi').classList.toggle('active');
     document.getElementById('icon-dropdown-giochi').classList.toggle('upset');
-});
+}, true);
 
-document.getElementById('dropdown-esperienze').addEventListener('click', function name() {
-    document.getElementById('dropdown-esperienze').classList.toggle('text-active-dropdown');
+document.getElementById('dropdown-esperienze').addEventListener('click', function openMenuEsperienze() {
     document.getElementById('dropdown-submenu-esperienze').classList.toggle('active');
+    document.getElementById('dropdown-esperienze').classList.toggle('text-active-dropdown');
     document.getElementById('icon-dropdown-esperienze').classList.toggle('upset');
-});
+}, true);
+
+
+document.getElementById('dropdown-informazioni').addEventListener('click', function openMenuInformazioni() {
+    document.getElementById('dropdown-submenu-informazioni').classList.toggle('active');
+    document.getElementById('dropdown-informazioni').classList.toggle('text-active-dropdown');
+    document.getElementById('icon-dropdown-informazioni').classList.toggle('upset');
+}, true);
+
+document.getElementById('dropdown-impegni').addEventListener('click', function openMenuImpegni() {
+    document.getElementById('dropdown-submenu-impegni').classList.toggle('active');
+    document.getElementById('dropdown-impegni').classList.toggle('text-active-dropdown');
+    document.getElementById('icon-dropdown-impegni').classList.toggle('upset');
+}, true);
+
+document.getElementById('dropdown-risorse').addEventListener('click', function openMenuRisorse() {
+    document.getElementById('dropdown-submenu-risorse').classList.toggle('active');
+    document.getElementById('dropdown-risorse').classList.toggle('text-active-dropdown');
+    document.getElementById('icon-dropdown-risorse').classList.toggle('upset');
+}, true);
