ca3eb7b5b770f2cf0787a64ddad697d568565bbb
Valerio Corda <corda.valerio05@GMAIL.COM>
2022-03-03 16:55:21 +0100

    create dropdown

---
+++ asset/main.js
@@ -0,0 +1,11 @@
+document.getElementById('dropdown-giochi').addEventListener('click', function name() {
+    document.getElementById('dropdown-giochi').classList.toggle('text-active-dropdown');
+    document.getElementById('dropdown-submenu-giochi').classList.toggle('active');
+    document.getElementById('icon-dropdown-giochi').classList.toggle('upset');
+});
+
+document.getElementById('dropdown-esperienze').addEventListener('click', function name() {
+    document.getElementById('dropdown-esperienze').classList.toggle('text-active-dropdown');
+    document.getElementById('dropdown-submenu-esperienze').classList.toggle('active');
+    document.getElementById('icon-dropdown-esperienze').classList.toggle('upset');
+});
